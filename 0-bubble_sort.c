f6c6b74b2dd4a77a1e8161559836a1fb3b3b1e01
hamza-25 <98708442+hamza-25@users.noreply.github.com>
2023-09-22 16:07:39 +0100

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,7 +11,7 @@ void bubble_sort(int *array, size_t size)
     size_t i, j;
     int tmp;
 
-    if (size == 1)
+    if (!array || size == 0)
         return;
     for (i = 0; i < size - 1; i++)
     {
