3368b64432d25f014cdb54f96d9172a83793efd3
hamza-25 <me677499@gmail.com>
2023-09-22 21:15:33 +0000

    add sort insertion doubly linkek list

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,59 +1,32 @@
 #include "sort.h"
 
 /**
- *
- *
- *
+ * insertion_sort_list - function that sort a doubly linked list
+ * @list: head of doubly linked list
 */
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current = *list, *tmp, *mov;
+	listint_t *current = *list, *tmp;
 
 	if (!list || !current)
 		return;
-	current = current->next;
 	while (current)
 	{
-		mov = current;
-		while (mov && mov->n < mov->prev->n)
+		while (current->next && current->n > current->next->n)
 		{
-			tmp = mov->prev;
-		/*if (mov->prev == NULL)
-		{
-			tmp = mov->prev;
-			mov->prev->next = mov->next;
-			mov->next->prev = tmp;
-			mov->next = tmp;
-			mov->prev = NULL
-			mov = tmp->next;
-			continue;
-		}
-		if (mov->next == NULL)
-		{
-			tmp = mov->prev;
-			mov->prev = mov->prev->prev;
-			mov->prev->prev->next = mov;
-			mov->prev->prev = mov;
-			mov->next = mov->prev;
-			mov->prev->next = NULL;
-			mov = tmp->next;
-			continue;
-		}*/
-	mov->prev = tmp->prev;
-         tmp->next = mov->next;
-
-            if (tmp->next != NULL)
-                tmp->next->prev = tmp;
-
-            mov->next = tmp;
-            tmp->prev = mov;
-
-            if (mov->prev != NULL)
-                mov->prev->next = mov;
-            else
-                *list = mov;
-		mov = tmp;
+			tmp = current->next;
+			current->next = tmp->next;
+			if (tmp->next)
+				tmp->next->prev = current;
+			tmp->prev = current->prev;
+			if (current->prev)
+				current->prev->next = tmp;
+			current->prev = tmp;
+			tmp->next = current;
+			if (!tmp->prev)
+				*list = tmp;
+			print_list(*list);
 		}
 	 current = current->next;
 	}
