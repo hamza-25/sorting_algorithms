76708fa9a5bab2fb2844d3cc113d4c035a77a01a
hamza-25 <me677499@gmail.com>
2023-09-23 11:27:54 +0000

    fix output

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,25 +10,20 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 	int tmp;
-	bool is_swap;
 
 	if (!array || size == 0)
 		return;
 	for (i = 0; i < size - 1; i++)
 	{
-		is_swap = false;
-		for (j = 0; j < size - 1 - i; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				tmp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = tmp;
-				is_swap = true;
+				print_array(array, size);
 			}
-			print_array(array, size);
 		}
-		if (is_swap == false)
-			break;
 	}
 }
