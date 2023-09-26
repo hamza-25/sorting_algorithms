6170b507733fae5c4bb3cf474c69620ebb03c07e
hamza-25 <me677499@gmail.com>
2023-09-26 16:45:08 +0000

    add counting sort

--- sort.h
+++ sort.h
@@ -28,5 +28,5 @@ int lomuto_partition(int *array, int low, int high, size_t size);
 void quick_sort(int *array, size_t size);
 void quick_sort_helper(int *array, int low, int high, size_t size);
 void shell_sort(int *array, size_t size);
-
+void counting_sort(int *array, size_t size);
 #endif
